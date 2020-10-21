c542f142930c991a8f5e78248dced9d37db0637a
Ed Finn <ed@iowacollegefoundation.org>
2020-10-20 19:30:02 -0500

    End of section 5

---
+++ client/src/app/jobs/jobs.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit {
+  registerMode = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
