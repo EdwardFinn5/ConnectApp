df67b9aa5bb3c83933de60e0df558bcde0dafe55
Ed Finn <ed@iowacollegefoundation.org>
2020-10-31 17:20:45 -0500

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -8,24 +8,22 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService,
-              private router: Router,
-              private toastr: ToastrService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/memberlist');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
@@ -33,5 +31,4 @@ export class NavComponent implements OnInit {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
-
 }
